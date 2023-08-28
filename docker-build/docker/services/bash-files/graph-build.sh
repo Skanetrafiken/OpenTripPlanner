b2433813f263464713096dae0f7a2721ff21356f
Henrik Abrahamsson <henrik.abrahamsson@decerno.se>
2023-08-28 09:06:18 +0000

    Add helm chart
    Helm chart that mostly uses the common-helm-charts templates.
    
    Update the post-merge-pipeline to build chart and deploy to dev.
    
    Uses workload identities so we don't have to convert it in a week or whatever.
    
    Related work items: #23105, #23111

--- docker-build/docker/services/bash-files/graph-build.sh
+++ docker-build/docker/services/bash-files/graph-build.sh
@@ -31,7 +31,7 @@ function buildGraphFromNetexData {
   file_path=$2
   graph_path=$3
   log_info "Building Graph"
-  if ! java -Xmx10500m -jar $otp_jar_path --save --build $graph_path; then
+  if ! java -Xmx${GRAPH_BUILD_MEMORY_MB:-10500}m -jar $otp_jar_path --save --build $graph_path; then
     log_error "Build OpenTripPlanner Graph database failed in bash $0"
     return 1
   fi
